e7f24940d4c7161a2f69b5242e3f56c8dd2e277c
floyd <ftmodau@gmail.com>
2018-09-27 15:14:27 +0200

    can work on both debian and rpm based hosts

---
+++ Global_Controller/commander.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+
+#Please ensure that the luser (custom user account ) has rights to perform certain functions and that the key is copied to all the required hostnames
+#To ensure security this script must only be run by root
+
+
+Check_ID() {
+  ID=`/usr/bin/whoami`
+  if [ $ID != "root" ]; then
+     echo "$ID, you must be root to run this administrative script."
+     exit 1
+  fi
+}
+
+luser={custom user without brackets}
+
+echo "Do you even bash bro?"
+echo ""
+read GIVENCOMMAND
+
+#for HOST in fqdn1 fqdn2 fqdn3 fqdn4 etc.....
+for HOST in {Please add hosts here like the example above}
+
+do
+
+
+        echo "host: $HOST"
+	echo "Result:"
+        ssh $luser@$HOST $GIVENCOMMAND
+        echo "RC=$?"
+        echo -e " "
+
+done
+
+Check_ID
