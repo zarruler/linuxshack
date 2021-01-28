13279db900a1e0976efb46177245a5029cb16fff
Floyd Modau <tebogofloyd.modau@vcontractor.co.za>
2021-01-28 20:52:36 +0200

    Added % mark

--- python_playbooks/percentage_increase.py
+++ python_playbooks/percentage_increase.py
@@ -8,4 +8,4 @@ current_salary = float(input("Enter your current salary: "))
 new_salary = float(input("Enter your new salary: "))
 
 #formula step by step
-print("Your salary has increased by", (( new_salary - current_salary ) / current_salary ) * 100)
+print("Your salary has increased by", (( new_salary - current_salary ) / current_salary ) * 100 , "%")
