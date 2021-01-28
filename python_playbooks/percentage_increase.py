9a4897c82d790ac57eec65ddf514ade5c770c438
Floyd Modau <tebogofloyd.modau@vcontractor.co.za>
2021-01-28 20:48:33 +0200

    Added percentage increase calculator

---
+++ python_playbooks/percentage_increase.py
@@ -0,0 +1,11 @@
+#Author: Floyd Modau
+#Date: Thu 28 Jan 2021 20:46:55 SAST
+
+
+print("Calculate increase in % after performance reviews")
+
+current_salary = float(input("Enter your current salary: "))
+new_salary = float(input("Enter your new salary: "))
+
+#formula step by step
+print("Your salary has increased by", (( new_salary - current_salary ) / current_salary ) * 100)
